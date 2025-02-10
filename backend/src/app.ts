3c96a47df2ca47888d55a94687a5623e570e1c77
Lukas Schmid <dimschlukas@gmail.com>
2025-02-10 20:29:46 +0100

    Setup CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import userRoutes from './routes/userRoutes.js';
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -11,8 +12,18 @@ const app = express();
 // Connect to database
 connectDB();
 
+// CORS options
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  methods: 'GET,PUT,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+  exposedHeaders: ['Authorization']
+};
+
 // Middleware
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // Routes
 app.use('/api/users', userRoutes);
