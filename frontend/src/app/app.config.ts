6459d4541b1bbc1f2bf8c5194bb005b994e1c6c3
Lukas Schmid <dimschlukas@gmail.com>
2025-02-10 21:42:39 +0100

    Install material UI

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './shared/interceptor/auth/token.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor]))
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideAnimationsAsync()
   ]
 };
