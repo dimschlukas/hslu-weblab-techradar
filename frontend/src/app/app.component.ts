a9adaed4cd77eefd46e41056dacf30b01e9a9b9c
Lukas Schmid <dimschlukas@gmail.com>
2025-02-10 23:28:27 +0100

    Create basic material appbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from './shared/services/auth/auth.service';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Techradar';
+
+  constructor(private authService: AuthService) {}
+
+  isLoggedIn = () => this.authService.isLoggedIn();
+  logout = () => this.authService.logout();
 }
