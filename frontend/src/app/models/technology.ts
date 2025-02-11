d28d7709b1a02d22911b1bdd0f3498e02b1382fe
Lukas Schmid <dimschlukas@gmail.com>
2025-02-11 23:42:07 +0100

    Add service to fetch and add technologies

---
+++ frontend/src/app/models/technology.ts
@@ -0,0 +1,8 @@
+export interface Technology {
+  id: string;
+  name: string;
+  ring: string;
+  category: string;
+  description: string;
+  justification: string;
+}
---
+++ frontend/src/app/shared/services/technologies/technologies.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environment/environment';
+import { catchError, Observable, of, throwError } from 'rxjs';
+import { Technology } from '../../../models/technology';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TechnologiesService {
+  url = environment.backendUrl + '/technologies';
+
+  constructor(private http: HttpClient) {}
+
+  getTechnologies(): Observable<Technology[]> {
+    return this.http
+      .get<Technology[]>(this.url, {
+        withCredentials: true
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  addTechnology(technology: Technology): Observable<Technology> {
+    return this.http
+      .post<Technology>(this.url, technology, {
+        withCredentials: true
+      })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'Something went wrong. Please try again later.';
+
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      console.error('Client-side error:', error.error.message);
+      errorMessage = error.error.message;
+    } else {
+      // Server-side error
+      console.error(`Backend returned code ${error.status}, body was:`, error.error);
+      errorMessage = error.error?.message || `Error Code: ${error.status}`;
+    }
+
+    return throwError(() => new Error(errorMessage));
+  }
+}
