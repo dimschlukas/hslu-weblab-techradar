252db9a16c94fef4dab954a0258ebdea2179667e
Lukas Schmid <dimschlukas@gmail.com>
2025-02-11 15:39:32 +0100

    Route between login, register and dashboard correctly based on sign in state

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { LoginComponent } from './routes/auth/login/login.component';
 import { RegisterComponent } from './routes/auth/register/register.component';
 import { DashboardComponent } from './routes/dashboard/dashboard.component';
 import { authGuard } from './shared/guards/auth/auth.guard';
+import { loggedInAuthGuard } from './shared/guards/auth/logged-in-auth.guard';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'login', component: LoginComponent, canActivate: [loggedInAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [loggedInAuthGuard] }
 ];
--- frontend/src/app/shared/guards/auth/logged-in-auth.guard.ts
+++ frontend/src/app/shared/guards/auth/logged-in-auth.guard.ts
@@ -1,12 +1,12 @@
-import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
+import { inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const loggedInAuthGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  if (!authService.isLoggedIn()) {
-    router.navigate(['/login']);
+  if (authService.isLoggedIn()) {
+    router.navigate(['/dashboard']);
     return false;
   }
   return true;
