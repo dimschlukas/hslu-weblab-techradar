6e2146344343db5102e6fac030182f0dee0eeafb
Lukas Schmid <dimschlukas@gmail.com>
2025-02-11 17:01:13 +0100

    Add guard for admin access

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,13 @@ import { RegisterComponent } from './routes/auth/register/register.component';
 import { DashboardComponent } from './routes/dashboard/dashboard.component';
 import { authGuard } from './shared/guards/auth/auth.guard';
 import { loggedInAuthGuard } from './shared/guards/auth/logged-in-auth.guard';
+import { AdministrationComponent } from './routes/administration/administration.component';
+import { adminAuthGuard } from './shared/guards/auth/admin-auth.guard';
 
 export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent, canActivate: [loggedInAuthGuard] },
-  { path: 'register', component: RegisterComponent, canActivate: [loggedInAuthGuard] }
+  { path: 'register', component: RegisterComponent, canActivate: [loggedInAuthGuard] },
+  { path: 'admin', component: AdministrationComponent, canActivate: [adminAuthGuard] },
+  { path: '', component: DashboardComponent, canActivate: [loggedInAuthGuard] }
 ];
---
+++ frontend/src/app/shared/guards/auth/admin-auth.guard.ts
@@ -0,0 +1,13 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { inject } from '@angular/core';
+
+export const adminAuthGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (!authService.isLoggedInAsAdmin()) {
+    router.navigate(['/dashboard']);
+    return false;
+  }
+  return true;
+};
