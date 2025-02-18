71a9dba5b47d408604b58118e5ef10f772549fdc
Lukas Schmid <dimschlukas@gmail.com>
2025-02-18 21:09:43 +0100

    Enhance user registration and login to be case-insensitive for email

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -7,10 +7,13 @@ import LoginLog from '../models/loginLogModel.js';
 export const registerUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const { email, password } = req.body;
-    const userExists = await User.findOne({ email: email });
+
+    const userExists = await User.findOne({
+      email: { $regex: email, $options: 'i' }
+    });
 
     if (userExists) {
-      const message = 'Invalid email or password';
+      const message = 'User already exists';
       res.status(400).json({ message });
       logging('register', false, email, req.ip, message);
       return;
@@ -34,7 +37,9 @@ export const registerUser = async (req: Request, res: Response): Promise<void> =
 export const loginUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email: email });
+    const user = await User.findOne({
+      email: { $regex: email, $options: 'i' }
+    });
 
     if (!user || !(await comparePass(password, user.password))) {
       const message = 'Invalid email or password';
